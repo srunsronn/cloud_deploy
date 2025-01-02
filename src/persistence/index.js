4632b8f07c7fcc633635ac8d82809368ae98cc82
srunsronn <158978515+srunsronn@users.noreply.github.com>
2025-01-02 12:57:31 +0700

    deploy

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -2,7 +2,4 @@ if (process.env.DB_HOST) {
     console.log('Using MySQL persistence.');
     module.exports = require('./mysql');
 }
-// else {
-//     console.warn('DB_HOST is not set. Falling back to SQLite.');
-//     module.exports = require('./sqlite');
-// }
+
